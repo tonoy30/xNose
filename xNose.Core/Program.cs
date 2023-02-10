f239e87ae4e4686995547603ed7dfa5bd1051ac1
tonoy30 <tonoy.sust@gmail.com>
2023-02-11 04:16:13 +0600

    unused import removed

--- xNose.Core/Program.cs
+++ xNose.Core/Program.cs
@@ -1,15 +1,10 @@
 ﻿using Microsoft.Build.Locator;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Symbols;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.MSBuild;
-using Microsoft.CodeAnalysis.Text;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace xNose.Core
